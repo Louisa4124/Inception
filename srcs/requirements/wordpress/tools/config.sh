8d693b4bd9d7eda55f727ee1c92f07d2ba0d5b28
Louiza Boudjemai <lboudjem@z4r4p7.42lyon.fr>
2023-11-21 17:03:51 +0100

    FIX : config.sh

--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-sleep 10 #make sure the db is launched
 if [ ! -f "/var/www/wordpress/wp-config.php " ];
 then
 	wp config create	--allow-root \
@@ -8,7 +7,8 @@ then
 						--dbuser=$DB_USR_NAME \
 						--dbpass=$DB_USR_PSWRD \
 						--dbhost=mariadb:3306 --path='/var/www/wordpress'
+else
+	echo "end"
 fi
 
-
-/usr/sbin/php-fpm${PHP_VERSION} -F
+/usr/sbin/php-fpm7.4 -F
