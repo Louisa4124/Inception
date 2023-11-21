51b47e606a98c3e3941511d78eec8a2c58495c48
Louiza Boudjemai <lboudjem@z4r4p7.42lyon.fr>
2023-11-21 16:44:37 +0100

    FIX : config.sh

--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -3,11 +3,12 @@
 sleep 10 #make sure the db is launched
 if [ ! -f "/var/www/wordpress/wp-config.php " ];
 then
-	wp core download --allow-root --path="/var/www/html/wordpress" \
-	&& wp config create	--dbname=$DB_NAME \
+	wp config create	--allow-root \
+						--dbname=$DB_NAME \
 						--dbuser=$DB_USR_NAME \
 						--dbpass=$DB_USR_PSWRD \
 						--dbhost=mariadb:3306 --path='/var/www/wordpress'
-else
-	echo "the end"
 fi
+
+
+/usr/sbin/php-fpm${PHP_VERSION} -F
