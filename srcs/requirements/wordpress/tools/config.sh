4a89672621f6b83d0b44824d63bec501ec88acb9
Louiza Boudjemai <lboudjem@z3r6p6.42lyon.fr>
2023-11-22 14:51:36 +0100

    FIX: wordpress container

--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-if [ ! -f "/var/www/wordpress/wp-config.php " ];
-then
-	wp config create	--allow-root \
-						--dbname=$DB_NAME \
-						--dbuser=$DB_USR_NAME \
-						--dbpass=$DB_USR_PSWRD \
-						--dbhost=mariadb:3306 --path='/var/www/wordpress'
-else
-	echo "end"
-fi
+wp core download --allow-root --path="/var/www/wordpress" \
+&& wp config create	--allow-root \
+					--dbname=$DB_NAME \
+					--dbuser=$DB_USR_NAME \
+					--dbpass=$DB_USR_PSWRD \
+					--dbhost=mariadb:3306 --path='/var/www/wordpress' \
+&& wp core install --allow-root --path="/var/www/wordpress" --url=$DOMAIN --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PSWRD --admin_email=$WP_ADMIN_EMAIL --skip-email \
+&& wp plugin update --allow-root --path="/var/www/wordpress" --all \
+&& wp theme install --allow-root --path="/var/www/wordpress" https://public-api.wordpress.com/rest/v1/themes/download/organizer.zip  --activate \
+&& wp user create --allow-root --path="/var/www/wordpress" $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PSWRD \
+&& chown www-data:www-data /var/www/wordpress/wp-content/uploads -R 
 
-/usr/sbin/php-fpm7.4 -F
+exec "$@"
