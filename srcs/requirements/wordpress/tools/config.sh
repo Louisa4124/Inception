d7ca904ae248bede3772d5722a5eeb19ba856f6a
Louiza Boudjemai <lboudjem@z3r6p4.42lyon.fr>
2023-11-23 16:15:09 +0100

    wip

--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -10,7 +10,7 @@ then
 						--dbhost=mariadb:3306 --path='/var/www/wordpress' \
 	&& wp core install --allow-root --path="/var/www/wordpress" --url=$DOMAIN --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PSWRD --admin_email=$WP_ADMIN_EMAIL --skip-email \
 	&& wp plugin update --allow-root --path="/var/www/wordpress" --all \
-	&& wp theme install --allow-root --path="/var/www/wordpress" https://downloads.wordpress.org/theme/ayageek.1.4.3.zip --activate \
+	&& wp theme install --allow-root --path="/var/www/wordpress" https://downloads.wordpress.org/theme/popularfx.1.2.4.zip --activate \
 	&& wp user create --allow-root --path="/var/www/wordpress" $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PSWRD \
 	&& chown www-data:www-data /var/www/wordpress/wp-content/uploads -R 
 else
