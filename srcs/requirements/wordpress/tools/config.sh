d29cc489cf15e5a7352cd9bf90be347f6d7975f8
Louiza Boudjemai <lboudjem@z3r6p4.42lyon.fr>
2023-11-23 14:11:03 +0100

    wip

--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-wp core download --allow-root --path="/var/www/html" \
+wp core download --allow-root --path="/var/www/wordpress" \
 && wp config create	--allow-root \
 					--dbname=$DB_NAME \
 					--dbuser=$DB_USR_NAME \
 					--dbpass=$DB_USR_PSWRD \
-					--dbhost=mariadb:3306 --path='/var/www/html' \
-&& wp core install --allow-root --path="/var/www/html" --url=$DOMAIN --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PSWRD --admin_email=$WP_ADMIN_EMAIL --skip-email \
-&& wp plugin update --allow-root --path="/var/www/html" --all \
-&& wp theme install --allow-root --path="/var/www/html" https://public-api.html.com/rest/v1/themes/download/organizer.zip  --activate \
-&& wp user create --allow-root --path="/var/www/html" $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PSWRD \
-&& chown www-data:www-data /var/www/html/wp-content/uploads -R 
+					--dbhost=mariadb:3306 --path='/var/www/wordpress' \
+&& wp core install --allow-root --path="/var/www/wordpress" --url=$DOMAIN --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PSWRD --admin_email=$WP_ADMIN_EMAIL --skip-email \
+&& wp plugin update --allow-root --path="/var/www/wordpress" --all \
+&& wp user create --allow-root --path="/var/www/wordpress" $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PSWRD \
+&& chown www-data:www-data /var/www/wordpress/wp-content/uploads -R 
 
 exec "$@"
